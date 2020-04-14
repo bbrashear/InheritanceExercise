479adabc7619926553137a09746e54f7ce9c7e6e
Blake Brashear <bbrashear94@gmail.com>
2020-04-14 13:28:36 -0400

    Completed all TDOs in the exercise

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -28,12 +28,46 @@ namespace Inheritance
              *  
              * Creatively display the class member values 
              */
+            Bird zapdos = new Bird()
+            {
+                Name = "Zapdos",
+                Age = 1000,
+                Legs = 2,
+                HasBackbone = true,
+                IsWarmBlooded = true,
+                HasFeathers = true,
+                Wings = 2,
+                Type = "electric",
+            };
+
+            Console.WriteLine($"{zapdos.Name} is an {zapdos.Type} pokemon that is {zapdos.Age} years old and has " +
+                $"{zapdos.Legs} legs and {zapdos.Wings} wings.");
+            Console.WriteLine($"Is {zapdos.Name} a vertebrate, warm blooded, and has feathers? " +
+                $"{zapdos.HasBackbone}, {zapdos.IsWarmBlooded}, and {zapdos.HasFeathers}");
+            Console.WriteLine();
 
             /*Create an object of your Reptile class
              *  give values to your members using the object of your Bird class
              *  
              * Creatively display the class member values 
              */
+            Reptile dragon = new Reptile()
+            {
+                Name = "Smaug",
+                Age = 1200,
+                Legs = 4,
+                HasBackbone = true,
+                IsColdBlooded = true,
+                HasScales = true,
+                Environment = "Erebor",
+                Defense = "breathes fire"
+            };
+
+            Console.WriteLine($"{dragon.Name} is a {dragon.Age} year old dragon that lives under {dragon.Environment}," +
+                $" {dragon.Defense}, and has {dragon.Legs} legs");
+            Console.WriteLine($"Is {dragon.Name} a vertebrate, cold blooded, and has scales?" +
+                $" {dragon.HasBackbone}, {dragon.IsColdBlooded}, and {dragon.HasScales}");
+            Console.WriteLine();
         }
     }
 }
